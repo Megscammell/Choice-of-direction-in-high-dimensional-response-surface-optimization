11a4afb12db165f5abe0e94be40d5f1d3317b4ea
Meg Scammell <scammellm@cardiff.ac.uk>
2021-09-03 10:52:58 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,8 @@ exec(open("src/est_dir/version.py", "r").read())
 requirements = ["numpy>=1.19.2", "scipy>=1.5.2", "pytest>=6.2.1",
                 "tqdm>=4.50.2", "setuptools>=49.2.1",
                 "hypothesis>=6.0.0", "matplotlib>=3.3.2",
-                "pytest-cov>=2.10.1"]
+                "pytest-cov>=2.10.1", "statsmodels>=0.12.0",
+                "seaborn>=0.11.0"]
 
 setup(
     name="est_dir",
